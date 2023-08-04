8ec3d41a2dfd317f1bf88357654e24ace7a46c3a
Daniel Bahia <booude@gmail.com>
2023-08-04 11:58:48 -0300

    First commit

---
+++ src/game.py
@@ -0,0 +1,56 @@
+import pygame as p
+from const import *
+from board import Board
+from dragger import Dragger
+
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.dragger = Dragger()
+
+    def show_bg(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row + col) % 2 == 0:
+                    color = (234, 235, 200)
+                else:
+                    color = (119, 154, 88)
+
+                rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+
+                p.draw.rect(surface, color, rect)
+
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = p.image.load(piece.texture)
+                        img_center = (
+                            col * SQSIZE + SQSIZE // 2,
+                            row * SQSIZE + SQSIZE // 2,
+                        )
+                        piece.texture_rect = img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
+
+    def show_moves(self, surface):
+        if self.dragger.dragging:
+            piece = self.dragger.piece
+
+            for move in piece.moves:
+                color = (
+                    "#C4C4C4"
+                    if (move.final.row + move.final.col) % 2 == 0
+                    else "#A4A4A4"
+                )
+                rect = (
+                    move.final.col * SQSIZE,
+                    move.final.row * SQSIZE,
+                    SQSIZE,
+                    SQSIZE,
+                )
+                p.draw.rect(surface, color, rect)
---
+++ src/main.py
@@ -0,0 +1,81 @@
+import pygame as p
+import sys
+
+from const import *
+from game import Game
+from square import Square
+from move import Move
+
+
+class Main:
+    def __init__(self):
+        p.init()
+        self.screen = p.display.set_mode((WIDTH, HEIGHT))
+        p.display.set_caption("Python Chess")
+        self.game = Game()
+
+    def mainloop(self):
+        screen = self.screen
+        game = self.game
+        board = self.game.board
+        dragger = self.game.dragger
+
+        while True:
+            game.show_bg(screen)
+            game.show_moves(screen)
+            game.show_pieces(screen)
+
+            if dragger.dragging:
+                dragger.update_blit(screen)
+
+            for event in p.event.get():
+                if event.type == p.MOUSEBUTTONDOWN:
+                    dragger.update_mouse(event.pos)
+
+                    clicked_row = dragger.mouseY // SQSIZE
+                    clicked_col = dragger.mouseX // SQSIZE
+
+                    if board.squares[clicked_row][clicked_col].has_piece():
+                        piece = board.squares[clicked_row][clicked_col].piece
+                        board.calc_moves(piece, clicked_row, clicked_col)
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
+
+                        game.show_bg(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
+
+                elif event.type == p.MOUSEMOTION:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
+                        dragger.update_blit(screen)
+
+                elif event.type == p.MOUSEBUTTONUP:
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        released_row = dragger.mouseY // SQSIZE
+                        released_col = dragger.mouseX // SQSIZE
+
+                        initial = Square(dragger.initial_row, dragger.initial_col)
+                        final = Square(released_row, released_col)
+                        move = Move(initial, final)
+
+                        if board.valid_move(dragger.piece, move):
+                            board.move(dragger.piece, move)
+                            game.show_bg(screen)
+                            game.show_pieces(screen)
+
+                    dragger.undrag_piece()
+
+                elif event.type == p.QUIT:
+                    p.quit()
+                    sys.exit()
+
+            p.display.update()
+
+
+main = Main()
+main.mainloop()
